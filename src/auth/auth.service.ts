8667de04df72429c461482f3b5293948f026ecf1
Phyo Thiha Aung <phyothiha050@gmail.com>
2024-09-10 13:42:41 +0630

    dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
-@Injectable({})
-export class AuthService {}
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  signup(dto: AuthDto) {
+    return { message: 'I am sign up' };
+  }
+
+  signin() {
+    return { message: 'I am sign in' };
+  }
+}
