15a3ea95eacc188f509a3a286b87f09ffd29dd23
Phyo Thiha Aung <phyothiha050@gmail.com>
2024-09-12 16:56:32 +0630

    Guard and decorator

---
+++ src/user/user.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @HttpCode(HttpStatus.OK)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
